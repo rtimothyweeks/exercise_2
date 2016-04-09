c5179e528f40bfa210fc9aef0efce72bd7696e2d
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 11:55:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 pip install psycopg2
 sparse quickstart 'EXT2Tweetwordcount'
-git clone https://github.com/UC-Berkeley-I-School/w205-spring-16-labs-exercises.git
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py EX2Tweetwordcount/src/spouts
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py EX2Tweetwordcount/src/bolts
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py EX2Tweetwordcount/src/bolts
