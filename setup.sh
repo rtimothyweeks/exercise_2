cee45b23489a3ea565ea7453601fd79286e46855
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 13:43:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ pip install psycopg2
 sparse quickstart EXTwoTweetwordcount
 cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py' 'EXTwoTweetwordcount/src/spouts/tweets.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py' 'EXTwoTweetwordcount/src/bolts/parse.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py' 'EXTwoTweetwordcount/src/bolts/wordcount.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/Twittercredentials.py' 'EXTwoTweetwordcount/Twittercredentials.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/hello-stream-twitter.py' 'EXTwoTweetwordcount/hello-stream-twitter.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/psycopg-sample.py' 'EXTwoTweetwordcount/psycopg-sample.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/topologies/tweetwordcount.clj' 'EXTwoTweetwordcount/topologies/tweetwordcount.clj'
 rm EXTwoTweetwordcount/topologies/*.clj
 cp 'exercise_2/EX2tweetwordcount.clj' 'EXTwoTweetwordcount/topologies/EX2tweetwordcount.clj'
+cp 'exercise_2/wordcount.py' 'EXTwoTweetwordcount/src/bolts/wordcount.py'
 pip install tweepy
+python 'exercise_2/create_db.py'
