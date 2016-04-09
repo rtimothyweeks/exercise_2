59632ecbebf20ee2b3610b63e57b9c6ffefa0e77
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 12:17:24 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+pip install --upgrade pip
 pip install psycopg2
 sparse quickstart EX2Tweetcount
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spounts/tweets.py' 'EX2Tweetcount/src/spounts/tweets.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py' 'EX2Tweetcount/src/spounts/tweets.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py' 'EX2Tweetcount/src/bolts/parse.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py' 'EX2Tweetcount/src/bolts/wordcount.py'
 cp 'w205-spring-16-labs-exercises/exercise_2/Twittercredentials.py' 'EX2Tweetcount/Twittercredentials.py'
