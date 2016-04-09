0b610619087fea25b76851acf16d330add2faa0d
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 13:20:13 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install psycopg2
-sparse quickstart EX2Tweetcount
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py' 'EX2Tweetcount/src/spouts/tweets.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py' 'EX2Tweetcount/src/bolts/parse.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py' 'EX2Tweetcount/src/bolts/wordcount.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/Twittercredentials.py' 'EX2Tweetcount/Twittercredentials.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/hello-stream-twitter.py' 'EX2Tweetcount/hello-stream-twitter.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/psycopg-sample.py' 'EX2Tweetcount/psycopg-sample.py'
-cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/topologies/tweetwordcount.clj' 'EX2Tweetcount/topologies/tweetwordcount.clj'
-rm EX2Tweetcount/topologies/*.clj
-cp 'exercise_2/EX2tweetwordcount.clj' 'EX2Tweetcount/topologies/EX2tweetwordcount.clj'
+sparse quickstart EXTwoTweetwordcount
+cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py' 'EXTwoTweetwordcount/src/spouts/tweets.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py' 'EXTwoTweetwordcount/src/bolts/parse.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py' 'EXTwoTweetwordcount/src/bolts/wordcount.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/Twittercredentials.py' 'EXTwoTweetwordcount/Twittercredentials.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/hello-stream-twitter.py' 'EXTwoTweetwordcount/hello-stream-twitter.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/psycopg-sample.py' 'EXTwoTweetwordcount/psycopg-sample.py'
+cp 'w205-spring-16-labs-exercises/exercise_2/tweetwordcount/topologies/tweetwordcount.clj' 'EXTwoTweetwordcount/topologies/tweetwordcount.clj'
+rm EXTwoTweetwordcount/topologies/*.clj
+cp 'exercise_2/EX2tweetwordcount.clj' 'EXTwoTweetwordcount/topologies/EX2tweetwordcount.clj'
 pip install tweepy
