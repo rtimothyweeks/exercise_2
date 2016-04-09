542f82e38e35cabf5f3cc41ed9bafc7a4d0e0f7c
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 11:30:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
-source /opt/py27environment/bin/activate
 pip install psycopg2
 sparse quickstart EXT2Tweetwordcount
-
 git clone https://github.com/UC-Berkeley-I-School/w205-spring-16-labs-exercises/
-
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/spouts/tweets.py EX2Tweetwordcount/src/spouts
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/parse.py EX2Tweetwordcount/src/bolts
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/src/bolts/wordcount.py EX2Tweetwordcount/src/bolts
@@ -11,6 +8,4 @@ cp w205-spring-16-labs-exercises/exercise_2/Twittercredentials.py EX2Tweetwordco
 cp w205-spring-16-labs-exercises/exercise_2/hello-stream-twitter.py EX2Tweetwordcount
 cp w205-spring-16-labs-exercises/exercise_2/psycopg-sample.py EX2Tweetwordcount
 cp w205-spring-16-labs-exercises/exercise_2/tweetwordcount/topologies/tweetwordcount.clj EX2Tweetwordcount/topologies
-
 pip install tweepy
-
