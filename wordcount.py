d9617908c26baa0c991d999ab7e9d8ba4fed4173
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-09 12:49:53 -0500

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -10,16 +10,19 @@ class WordCounter(Bolt):
     def initialize(self, conf, ctx):
         self.counts = Counter()
         self.redis = StrictRedis()
+        self.conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+        self.cur = self.conn.cursor()
 
     def process(self, tup):
         word = tup.values[0]
-
+        
         # Write codes to increment the word count in Postgres
-        # Use psycopg to interact with Postgres
-        # Database name: Tcount
-        # Table name: Tweetwordcount
-        # you need to create both the database and the table in advance.
-
+        # Get and increment current db count
+        count = self.cur.execute("select count from Tweetwordcount where word=%s",word) + 1
+        
+        # Commit count change to db
+        self.cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s", (word, count))
+        self.conn.commit()
 
         # Increment the local count
         self.counts[word] += 1
