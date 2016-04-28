f08d28507483cbd5afbd831a8ef3e4fc607f971f
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-27 22:39:25 -0500

    Create finalresults.py

---
+++ finalresults.py
@@ -0,0 +1,14 @@
+import sys
+import psycopg2
+
+con = psycopg2.connect(database'postgres')
+cur = con.cursor()
+
+def qry(word):
+  if word is None:
+    q = ''
+  else:
+    count = cur.execute("select count from Tweetwordcount where word='%s';" % word)
+  return "Total number of occurences of '%s': %s" % (word, count))
+  
+qry(sys.argv[1])
