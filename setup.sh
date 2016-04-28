be09d7b4406661190c28fff234b80cd309a7052e
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-27 20:37:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ mount -t ext4 /dev/xvdf/ /data
 bash /data/start_postgres.sh
 mv /usr/bin/python /usr/bin/python266
 ln -s /usr/bin/python2.7 /usr/bin/python
+sudo curl -o ez_setup.py https://bootstrap.pypa.io/ez_setup.py
+python ez_setup.py
+/usr/bin/easy_install-2.7 pip
 pip install virtualenv
 pip install streamparse
 pip install --upgrade pip
