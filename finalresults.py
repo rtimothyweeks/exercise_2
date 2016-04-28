fb81b7b6fb1d35e7394bef91719c182f0fdf3de7
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-27 22:48:09 -0500

    Update finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -4,14 +4,16 @@ import psycopg2
 con = psycopg2.connect(database='postgres')
 cur = con.cursor()
 
-def qry(word):
-  if word is None:
-    q = ''
-  else:
-    q = "select count from Tweetwordcount where word='%s';" % word
-    print q
-    count = cur.execute(q)
-    print count
-  print "Total number of occurences of '%s': %s" % (word, count)
+t = cur.execute('select * from Tweetwordcount limit 20;')
+print t
+# def qry(word):
+#   if word is None:
+#     q = ''
+#   else:
+#     q = "select count from Tweetwordcount where word='%s';" % word
+#     print q
+#     count = cur.execute(q)
+#     print count
+#   print "Total number of occurences of '%s': %s" % (word, count)
   
-qry(sys.argv[1])
+# qry(sys.argv[1])
