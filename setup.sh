9ffcfc70899cc567a4d97dc8522c25470d53ffc4
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-27 21:27:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -s /usr/bin/python2.7 /usr/bin/python
 sudo curl -o ez_setup.py https://bootstrap.pypa.io/ez_setup.py
 python ez_setup.py
 /usr/bin/easy_install-2.7 pip
+pip install redis
 pip install virtualenv
 pip install streamparse
 pip install --upgrade pip
