617db80f8076977cdfc54a2ba04b6b4331e164a6
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-29 16:34:45 -0500

    Update finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -12,7 +12,7 @@ def qry(argv):
     cur.execute(q)
     print cur.fetchone()
   else:
-    word = argv[1]
+    word == argv[1]
     q = "select count from Tweetwordcount where word='%s';" % word
     # print q
     cur.execute(q)
