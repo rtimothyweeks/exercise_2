5f960d2e3517f141991ff51ffe7592865746f2e1
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-29 17:14:11 -0500

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -7,7 +7,7 @@ cur = con.cursor()
 if len(sys.argv) < 2:
   print 'Insufficient arguments provided'
   sys.exit()
-
+print sys.argv
 start = sys.argv[1]
 end = sys.argv[2]
 q = 'select word, count from Tweetwordcount where count between %s and %s order by count desc;' % (start,end)
