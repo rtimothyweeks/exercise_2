932a55415032c0cb6afc5acb199e719c59ca20dc
rtimothyweeks <rtimothyweeks@icloud.com>
2016-04-29 16:48:14 -0500

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -3,9 +3,10 @@ import psycopg2
 conn = psycopg2.connect(database="postgres")
 cur = conn.cursor()
 
-word = 'RTW_test2'
+word = 'RTW_test5'
 cur.execute("INSERT into Tweetwordcount VALUES ('%s',989);" % word)
 conn.commit()
 cur.execute("select * from Tweetwordcount where word='%s';" % word)
+print cur.fetchone()
 print cur.fetchall()
 conn.close()
